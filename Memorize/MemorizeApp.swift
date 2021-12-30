5ed07c45a549c67b30a86a550cd331625aab13e5
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2021-12-30 13:34:55 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Julian Grunauer on 12/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
